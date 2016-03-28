4b1a3bcbe666b76d5c56ef3c7ce10d036bd3b453
Evan Waters <evan.waters@gmail.com>
2016-03-27 17:41:17 -0700

    We can play audio

--- Gabb/PlayEpisodeTableViewController.swift
+++ Gabb/PlayEpisodeTableViewController.swift
@@ -7,19 +7,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlayEpisodeTableViewController: UITableViewController {
     
     var podcast:NSDictionary!
     var episode:NSDictionary!
+    var player:AVPlayer!
+    var audioPlaying:Bool = false
 
     @IBOutlet weak var podcastImageView: UIImageView!
     @IBOutlet weak var podcastTitle: UILabel!
     @IBOutlet weak var episodeTitle: UILabel!
+    @IBOutlet weak var playButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         formatView()
+        initializePlayer()
     }
     
     func formatView() {
@@ -33,6 +38,14 @@ class PlayEpisodeTableViewController: UITableViewController {
             self.episodeTitle.text = episodeTitle
         }
     }
+    
+    func initializePlayer() {
+        if let audioUrl = episode["audio_url"] as? String {
+            if let url = NSURL(string: audioUrl) {
+                player = AVPlayer(URL: url)
+            }
+        }
+    }
 
     // MARK: - Table view data source
 
@@ -44,12 +57,49 @@ class PlayEpisodeTableViewController: UITableViewController {
         return 3
     }
     
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0
+    }
+    
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         switch (indexPath.row) {
         case 0:
             return screenSize.width
         default:
             return 80.0
+        }
+    }
+    
+    // MARK: - User Actions
+    
+    @IBAction func playButtonTapped(sender: AnyObject) {
+        if audioPlaying {
+            pauseEpisode()
+        }
+        else {
+            playEpisode()
+        }
+    }
+    
+    // MARK: - Private
+    
+    private func playEpisode() {
+        if let pauseImage = UIImage(named: "pause") {
+            playButton.setImage(pauseImage, forState: .Normal)
+        }
+        if let player = player {
+            player.play()
+            audioPlaying = true
+        }
+    }
+    
+    private func pauseEpisode() {
+        if let playImage = UIImage(named: "play") {
+            playButton.setImage(playImage, forState: .Normal)
+        }
+        if let player = player {
+            player.pause()
+            audioPlaying = false
         }
     }
 
