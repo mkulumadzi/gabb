8a50e58fa759d7f802966c672a12448f5acef6e4
Evan Waters <evan.waters@gmail.com>
2016-04-10 17:28:47 -0700

    Got a chat item model sorta working

--- Gabb/ChatItem.swift
+++ Gabb/ChatItem.swift
@@ -10,22 +10,32 @@ import Foundation
 import Chatto
 import ChattoAdditions
 
-//class ChatItem:NSObject, ChatItemProtocol {
-//    
-//    var type:ChatItemType = TextMessageModel.chatItemType
-//    private var _uid:String!
-//    
-//    init(type:ChatItemType, uid: String) {
-//        _uid = uid
-//    }
-//    
-//    var uid:String {
-//        if _uid != nil {
-//            return _uid
-//        }
-//        else { // Not sure why I have to do this, but I seem to have to make this value always available
-//            return ""
-//        }
-//    }
-//    
-//}
+class ChatItem:NSObject, ChatItemProtocol {
+    
+    private var _type:ChatItemType!
+    private var _uid:String!
+    
+    init(type:ChatItemType, uid: String) {
+        _uid = uid
+        _type = type
+    }
+    
+    var uid:String {
+        if _uid != nil {
+            return _uid
+        }
+        else { // Not sure why I have to do this, but I seem to have to make this value always available
+            return ""
+        }
+    }
+    
+    var type:ChatItemType {
+        if _type != nil {
+            return _type
+        }
+        else {
+            return ""
+        }
+    }
+    
+}
